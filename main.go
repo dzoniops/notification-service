c6ad1dec6bb5a8d01e04375b49d036d3b79b0aa6
unknown <s.nino.petrovic@gmail.com>
2023-08-01 15:10:41 +0200

    feat: added connect for local mongo db and basic models

--- main.go
+++ main.go
@@ -2,29 +2,21 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"os"
-
+	"github.com/dzoniops/notification-service/db"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
-	uri := fmt.Sprintf("mongodb+srv://%s:%s@cluster06443.hcrrodp.mongodb.net/?retryWrites=true&w=majority", os.Getenv("MONGO_USERNAME"), os.Getenv("MONGO_PASSWORD"))
-	// Use the SetServerAPIOptions() method to set the Stable API version to 1
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
-
+	//uri := fmt.Sprintf("mongodb+srv://%s:%s@cluster06443.hcrrodp.mongodb.net/?retryWrites=true&w=majority", os.Getenv("MONGO_USERNAME"), os.Getenv("MONGO_PASSWORD"))
+	uri := "mongodb://localhost:27017"	
 	// Create a new client and connect to the server
-	client, err := mongo.Connect(context.TODO(), opts)
+	client, err := db.InitDb(uri) 
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to connect to Mongodb:",err)	
 	}
 
 	defer func() {
@@ -32,10 +24,11 @@ func main() {
 			panic(err)
 		}
 	}()
-
-	// Send a ping to confirm a successful connection
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
-		panic(err)
-	}
-	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	
+	db.InsertData(client)
+	// // Send a ping to confirm a successful connection
+	// if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 }
